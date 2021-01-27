c69a35fef5630e94751ecf4ce9025283152630ec
qlhan <qlhan@incarcloud.com>
2021-01-27 09:46:58 +0800

    0.5.0-SNAPSHOT ValidBean

--- hello-pojo/src/main/java/com/example/hello/pojo/ValidBean.java
+++ hello-pojo/src/main/java/com/example/hello/pojo/ValidBean.java
@@ -5,10 +5,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
 
 /**
  * 测试验证Bean
@@ -34,6 +32,30 @@ public class ValidBean {
     private String notEmpty = "";
 
     @NotBlank(message = "被注释的字符串的必须非空，不能是空白字符串")
-    @ApiModelProperty(position = 2, value = "非空白字符串")
+    @ApiModelProperty(position = 4, value = "例如：username")
     private String notBlank = "\t";
+
+    @AssertTrue(message = "被注释的元素必须为true")
+    @ApiModelProperty(position = 5, value = "暂无")
+    private boolean assertTrue = false;
+
+    @AssertFalse(message = "被注释的元素必须为false")
+    @ApiModelProperty(position = 6, value = "暂无")
+    private boolean assertFalse = true;
+
+    @Min(value = 18, message = "被注释的元素必须是一个数字，其值必须大于等于指定的最小值")
+    @ApiModelProperty(position = 7, value = "例如：age")
+    private long min = 10;
+
+    @Max(value = 120, message = "被注释的元素必须是一个数字，其值必须小于等于指定的最大值")
+    @ApiModelProperty(position = 8, value = "例如：age")
+    private long max = 140;
+
+    @DecimalMin(value = "0", inclusive = false, message = "被注释的元素必须是一个数字，其值必须大于等于指定的最小值")
+    @ApiModelProperty(position = 9, value = "例如：salary")
+    private BigDecimal decimalMin = BigDecimal.ZERO;
+
+    @DecimalMax(value = "0", inclusive = false, message = "被注释的元素必须是一个数字，其值必须小于等于指定的最大值")
+    @ApiModelProperty(position = 10, value = "例如：salary")
+    private BigDecimal decimalMax = BigDecimal.ZERO;
 }
