e5154b2eba396a9090dcbbc88c336dc5303c2ad4
qlhan <qlhan@incarcloud.com>
2021-02-08 12:03:14 +0800

    0.5.0-SNAPSHOT spring validation

--- hello-web-admin/src/main/java/com/example/hello/web/exception/ExceptionControllerAdvice.java
+++ hello-web-admin/src/main/java/com/example/hello/web/exception/ExceptionControllerAdvice.java
@@ -5,10 +5,15 @@ import com.example.hello.data.ApiData;
 import com.example.hello.data.ApiException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 全局异常控制器建议
@@ -27,6 +32,21 @@ public class ExceptionControllerAdvice {
         return new ApiData<String>()
                 .setCode(e.getCode())
                 .setErrorMessage(e.getMessage());
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiData<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        Map<String, String> tips = new HashMap<>(e.getBindingResult().getAllErrors().size());
+        bindingResult.getFieldErrors().forEach(error -> {
+            tips.put(error.getField(), error.getDefaultMessage());
+        });
+        return new ApiData<Map<String, String>>()
+                .setCode(600)
+                .setErrorMessage("valid error")
+                .setData(tips);
     }
 
     @ResponseBody
