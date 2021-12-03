a700674c9494666260e86aae216313c305c68fc7
qlhan <qlhan@incarcloud.com>
2021-12-03 11:33:33 +0800

    0.14.0-SNAPSHOT onlyoffice integration

--- hello-web-doc/src/main/java/com/onlyoffice/integration/IntegrationConfiguration.java
+++ hello-web-doc/src/main/java/com/onlyoffice/integration/IntegrationConfiguration.java
@@ -32,7 +32,7 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class IntegrationConfiguration {
 
-    @Value("${files.storage}")
+    @Value("${files.storage.address}")
     private String storageAddress;
 
     @Autowired
